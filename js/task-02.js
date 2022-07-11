2e47524bcd831f9368d58e710841e9ea7668dd55
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-07-11 15:14:44 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elements = ingredients.map(ingredient => {
+  
+  const liElem = document.createElement('li');
+  liElem.textContent = ingredient;
+  liElem.classList.add('item');
+  
+  return liElem;
+  });
+
+const list = document.querySelector('#ingredients');
+list.append(...elements);
