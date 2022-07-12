2669ff26813866a79ceda0f48a9bf71bc612b482
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-07-12 17:19:09 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const colorNameEl = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorNameEl.textContent = color;
+}
